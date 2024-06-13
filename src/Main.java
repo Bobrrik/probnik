efd1486e111b53a8f56dff3b9018e8565b7723bc
Bobr <Ziroy2017@yandex.ru>
2024-06-13 22:04:14 +0300

    :0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Tank tank = new Tank();
+        tank.goForward(10);
+        tank.printPosition();
+        tank.turnRight();
+        tank.goBackward(100);
+        tank.printPosition();
 
     }
 }
